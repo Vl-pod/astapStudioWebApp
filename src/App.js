d958f986e58859bee58dbd08056931574a22e199
Vl-pod <podsevalow@gmail.com>
2023-08-09 02:23:26 +0400

    started create advantages slider

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Advantages from './components/Advantages/Advantages__mobile.jsx';
 import Header from './components/Header__mobile/Header__mobile.jsx'
 import Promo from './components/Promo__mobile/Promo__mobile.jsx';
 
@@ -7,6 +8,7 @@ function App() {
 		<div className = "App">
 			<Header />
 			<Promo />
+			<Advantages />
 		</div>
 	);
 }
