eedeb03a5332f637e43a7757f43883d7d2196ff1
Vl-pod <podsevalow@gmail.com>
2023-08-09 17:16:43 +0400

    Advantages changed

--- src/components/Advantages/Advantages__mobile.jsx
+++ src/components/Advantages/Advantages__mobile.jsx
@@ -2,9 +2,28 @@ import './Advantages__mobile.css'
 
 const Advantages = () => {
 	return ( 
-		<section className="advantages wrapper">
+		// className="advantages" = root
+		<section className="advantages">
+			<div className="wrapper">
+				<div className="content">
+					<div className="advantages__card">
+						<div className="advantages__card-content container">
+							<div className="advantages__card-header">
+								<h1>convenience</h1>
+								<div className="slider__buttons">slider</div>
+							</div>
+							<div className="advantages__card-footer">
+								<div className="advantages__card-footer--text">
+									<p>we<span></span>work<span></span>all<br />over the world</p>
+								</div>
+								<div className="advantages__card-footer--icon"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
-	 );
+	);
 }
  
 export default Advantages;
